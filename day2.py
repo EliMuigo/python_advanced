8f5b16250342f2ba018974bc868b1ae0bde291f7
EliMuigo <ellymuigo@gmail.com>
2023-08-10 11:55:19 +0300

    second commit

---
+++ day2.py
@@ -0,0 +1,64 @@
+#Creating a function and calling it
+"""def favorite_book(title):
+    print(f"My favorite book is,{title}!")
+
+favorite_book("Alice in borderland")
+
+#Passing Arguments
+def make_shirt(shirt_text,shirt_size = "large"):
+    print(f"/n The size of the shirt is {shirt_size} and the message is {shirt_text}!")
+
+make_shirt(shirt_text="I love python")
+
+#returning a dictionary
+def make_album(name,title,number_of_songs=None):
+    album = {'album_name':name,'album_title':title}
+    if number_of_songs is not None:
+        album['number_of_songs']=number_of_songs
+    return album
+
+album_1 = make_album('sam','love')
+album_2 = make_album('Adele','Dark',number_of_songs=20)
+print(album_1)
+print(album_2)"""
+
+def make_album(album_artist,album_title,number_of_songs=None):
+    musician_discography=f"{'album_artist':album_artist,'album_title':album_title}"
+    if number_of_songs is not None:
+        musician_discography['number_of_songs']=number_of_songs
+    return musician_discography
+def main():
+    print("Enter album information or type quit to exit.")
+    while True:
+        album_artist = input("Enter the artist's name: ")
+        if album_artist.lower()=='quit':
+            break
+
+        album_title = input("Enter the name of the album: ")
+        if album_title.lower()=='quit':
+            break
+
+        number_of_songs = input("Enter the number of albums:")
+        if number_of_songs.lower()=='quit':
+            break
+        
+        elif number_of_songs.isdigit():
+            number_of_songs = int(number_of_songs)
+            musician_discography = make_album(album_artist,album_artist,number_of_songs)
+        else:
+            musician_discography = make_album(album_artist,album_title)
+
+        print("Album Information")
+        print(musician_discography)
+if __name__=="__main__":
+    main()
+
+
+
+
+
+
+
+
+    
+
---
+++ graphics.py
@@ -0,0 +1,128 @@
+import turtle
+turtle.shape("turtle")
+for i in range(0,5):
+    turtle.forward(100)
+    turtle.right(72)
+
+turtle.exitonclick()
+
+import turtle
+src=turtle.Screen()
+src.bgcolor("red")
+turtle.color("blue","blue")
+for i in range(0,10):
+    turtle.right(36)
+    for i in range(0,5):
+        turtle.forward(100)
+        turtle.right(72)
+turtle.exitonclick()
+
+#square
+import turtle
+turtle.shape("turtle")
+src=turtle.Screen()
+src.bgcolor("black")
+turtle.color("white","white")
+for i in range(0,4):
+    turtle.right(90)
+    turtle.forward(300)
+    
+
+turtle.exitonclick()
+
+#triangle
+import turtle
+src=turtle.screensize(60,80)
+for i in range(0,3):
+    turtle.left(120)
+    turtle.forward(100)
+turtle.exitonclick()
+
+import turtle
+
+sr=turtle.Turtle()
+sr.color("pink","red")
+sr.begin_fill()
+sr.pensize(5)
+sr.left(50)
+sr.forward(133)
+sr.circle(50,200)
+sr.right(140)
+sr.circle(50,200)
+sr.forward(133)
+sr.end_fill()
+
+turtle.done()
+
+import turtle
+turtle.color("black","red")
+turtle.begin_fill()
+for i in range(0,4):
+    turtle.forward(70)
+    turtle.right(90)
+turtle.penup()
+turtle.end_fill()
+turtle.forward(100)
+
+turtle.pendown()
+turtle.color("black","yellow")
+turtle.begin_fill()
+for i in range(0,4):
+    turtle.forward(70)
+    turtle.right(90)
+turtle.penup()
+turtle.end_fill()
+turtle.forward(100)
+
+turtle.pendown()
+turtle.color("black","green")
+turtle.begin_fill()
+for i in range(0,4):
+    turtle.forward(70)
+    turtle.right(90)
+turtle.end_fill()
+
+turtle.exitonclick()
+
+import turtle
+
+turtle.bgcolor("pink")
+turtle.color("black")
+turtle.left(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.penup()
+turtle.forward(50)
+turtle.pendown()
+turtle.forward(75)
+turtle.right(90)
+turtle.forward(50)
+turtle.right(90)
+turtle.forward(75)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(75)
+turtle.penup()
+turtle.forward(75)
+turtle.pendown()
+turtle.forward(75)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(45)
+turtle.left(180)
+turtle.forward(45)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(75)
+
+turtle.hideturtle()
+turtle.exitonclick()
+
+import turtle
+for i in range(0,5):
+    turtle.forward(100)
+    turtle.right(144)
+turtle.exitonclick()
